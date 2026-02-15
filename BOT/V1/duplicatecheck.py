bdee5d65f5dc4f2808f63c7c547171f61fa1c3d1
Anjan Mahapatra <anjanmahapatra10@gmail.com>
2026-02-16 02:08:34 +0530

    Create duplicatecheck.py

---
+++ BOT/V1/duplicatecheck.py
@@ -0,0 +1,41 @@
+
+import requests
+import json
+import os
+
+database = os.environ["DATABASE_TOKEN"]
+print(database)
+url = "https://api.notion.com/v1/databases/" + str(database) + "/query"
+
+def doesItExist(link):
+  payload = json.dumps({
+    "filter": {
+      "property": "URL",
+      "url":{
+        "equals": link
+      }
+    }
+  })
+  headers = {
+    'Authorization': str(os.environ["AUTH_KEY"]),
+    'Notion-Version': '2021-05-13',
+    'Content-Type': 'application/json'
+  }
+  response = requests.post(url, headers=headers, data=payload)
+  try:
+    result = response.json()["results"]
+  except:
+    return False
+  if(len(result) == 0):
+    return False
+  return True
+
+def amIThere(file):
+    filesUploaded = []
+    with open("./dataUploaded.txt") as log:
+        filesUploaded = [line.strip() for line in log]
+    print(filesUploaded)
+
+    if file in filesUploaded:
+        return True
+    return False
